bb70b5cec646a80222b4bca686db3dfa80f6d3ed
Garrett Rapport <grrapport@gmail.com>
2019-07-21 19:11:06 -0500

    starting on model testing with 538 model

---
+++ ModelBuilder.py
@@ -0,0 +1,13 @@
+import csv
+
+with open('mlb_elo_2018.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            print(f'\t{row[0]} {row[1]} {row[2]}')
+            line_count += 1
+    print(f'Processed {line_count} lines.')
