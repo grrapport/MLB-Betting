8e05271c734d110f2f1b40743b14ba609162669d
Garrett Rapport <grrapport@gmail.com>
2019-08-18 22:09:48 -0500

    adding functions to get lines directly and then compare to target lines

--- GetTargetLines.py
+++ GetTargetLines.py
@@ -3,14 +3,30 @@ import csv
 import datetime
 import GameOdds
 import requests
+import xml.etree.ElementTree as ET
+
+
+def get_bookmaker_odds(response):
+    root = ET.fromstring(response)
+    for child in root.findall("./Leagues/league"):
+        if child.attrib['IdLeague'] == "5":
+            mlb_elem = child
+            break
+
+    for child in mlb_elem.findall("./game"):
+        print(child.attrib)
+
 
 five38_url = "https://projects.fivethirtyeight.com/mlb-api/mlb_elo_latest.csv"
-
-five38_response = requests.get(five38_url)
+bookmaker_url = "http://lines.bookmaker.eu/"
 
 prediction_data = []
+available_lines = []
+
 target_adv = 0.075
 tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+
+five38_response = requests.get(five38_url)
 with open('mlb_elo_latest.csv', 'wb') as file:
     file.write(five38_response.content)
 
@@ -20,7 +36,6 @@ with open('mlb_elo_latest.csv') as csv_file:
     line_count = 0
     for row in csv_reader:
         if line_count == 0:
-            #print(f'Column names are {", ".join(row)}')
             line_count += 1
         else:
             prediction_data.append(Five38MLBData.Five38MlbDataPoint(row))
@@ -29,9 +44,13 @@ print("Advantage: "+str(target_adv*100)+"%")
 print("*********************************************")
 print("")
 
+bookmaker_response = requests.get(bookmaker_url)
+get_bookmaker_odds(bookmaker_response.content)
+
+
 for prediction in prediction_data:
     if datetime.datetime.strptime(prediction.date, '%Y-%m-%d').date() != tomorrow:
         continue
     home_target_line = GameOdds.imp_prob_to_odds(float(prediction.rating_prob1) - target_adv)
     away_target_line = GameOdds.imp_prob_to_odds(float(prediction.rating_prob2) - target_adv)
-    print(prediction.home_team+"  "+ str(home_target_line)+"              "+prediction.away_team+"  "+str(away_target_line))
+    print(prediction.home_team+"  " + str(home_target_line)+"              "+prediction.away_team+"  "+str(away_target_line))
